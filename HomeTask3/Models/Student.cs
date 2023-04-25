ab2a47120fde0bb2037e36dbf14cb67bc881a60e
Shalala <nasirlishalala1011@gmail.com>
2023-04-25 08:34:04 +0400

    hometask done

---
+++ HomeTask3/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace HomeTask3.Models;
+
+internal class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public double Point;
+    public bool IsGraduated;
+
+    public Student(string name, string surname, string group, double point, bool isGraduated)
+    {
+        Name = name;    
+        Surname = surname;  
+        Group = group;
+        Point = point;
+        IsGraduated = isGraduated;
+    }
+
+    public string FullName()
+    {
+        return $"{Name} {Surname}";
+    }
+
+    public string Info()
+    {
+        return $"{FullName()} {Group} - {Point} point : {Graduated()}";
+    }
+
+    public string Graduated()
+    {
+        return IsGraduated ? "graduated" : "still studying";
+    }
+
+    public string SecondChance()
+    {
+        return (Point > 80) ? "eligible" : "not eligible";
+    }
+}
---
+++ HomeTask3/Program.cs
@@ -0,0 +1,7 @@
+﻿using HomeTask3.Models;
+
+Student me = new Student("Shalala", "Nasirli", "P137", 88, false);
+
+Console.WriteLine($"{me.Info()}");
+Console.WriteLine($"{me.FullName()}");
+Console.WriteLine($"{me.SecondChance()}");
